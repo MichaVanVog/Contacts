d0f48a877d379afd6a2cd9668ff11f316212dcf0
MichaVanVog <ipbw@mail.ru>
2025-01-27 20:19:27 +0300

    AddContactPage and EditContactPage added

---
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace Contacts.Maui.Views;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnEditContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(EditContactPage));
+    }
+
+    private void btnAddContact_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+
+    private void btnCancel_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
